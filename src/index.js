5dc8944bf326f65d1a2e5f47ba4555b620f395e5
Vadim Ulitsky <ulitsky.vadim@gmail.com>
2019-03-18 19:07:21 +0300

    feat: add check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arrBrackets = str.split("");
+
+  for (let i = 0; i < arrBrackets.length; i++) {
+    let firstBracket = arrBrackets[i];
+    let secondBracket = arrBrackets[i + 1];
+    bracketsConfig.forEach(configBrackets => {
+      if (
+        firstBracket == configBrackets[0] &&
+        secondBracket == configBrackets[1]
+      ) {
+        arrBrackets.splice(i, 2);
+        i -= 2;
+      }
+    });
+  }
+
+  if (arrBrackets.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
